01678056dbbe4307419891e1e1bc131848589be1
Lucan F <dapps911@hotmail.com>
2019-08-28 17:29:54 -0400

    fetched all 12 employees to display

--- js/app.js
+++ js/app.js
@@ -1,25 +1,45 @@
-const div = document.querySelector('.search-container');
+const div = document.querySelector('.gallery');
 
 
-fetch ('https://randomuser.me/api') 
+fetch ('https://randomuser.me/api/?results=12') 
   .then( (response) => {
     return response.json();
 
   })
   .then( (data) => {
-    const user = data.results[0];
-    // console.log(user);
-    const output = document.createElement('div');
-    output.innerHTML = `
-      <h2>${user.name.first}</h2>
-      <h3>${user.name.last}</h3>
-      <h4>${user.name.title}</h4>
-    
-    `
-    div.appendChild(output);
+    console.log(data.results)
+    // debugger;
+    const users = data.results;
+    users.forEach( (user) => {
+      const output = document.createElement('div');
+      output.innerHTML = 
+      `
+      
+        <div class="card-img-container">
+            <img class="card-img" src="${user.picture.medium}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+            <p class="card-text">${user.email}</p>
+            <p class="card-text cap">${user.location.city} ${user.location.state} </p>
+        </div>
      
-  })
+      `
+      output.classList.add('card');
+      div.appendChild(output);
+     
+   
+
+    })
+  });
 
   // .catch( (error) => {
   //   console.log('Opps something went wrong!');
   // });
+
+
+  // // A way of fetching all data
+  // function fetchData(url){
+  //   return fetch(url)
+  //     .then(res => res.json())
+  // }
