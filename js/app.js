4f53c7c07f29585dbf383fa0dab359da0076feb5
Lucan F <dapps911@hotmail.com>
2019-08-30 15:29:19 -0400

    add all comments

--- js/app.js
+++ js/app.js
@@ -3,20 +3,21 @@ const gallery = document.querySelector('.gallery');
 const modalContainer = document.createElement('div');
 
 modalContainer.classList.add('modal-container');
-
+// Fecth 12 random users from API
 fetch ('https://randomuser.me/api/?results=12') 
   .then( (response) => {
-    return response.json();
+    return response.json(); // return Promise as a JSON object
   })
   .then( (data) => {
-
     // const response2 = data.clone();
-    
+    // place JSON object into 'users' variable
     const users = data.results;
+    // Loop through each user in users array
     users.forEach( (user) => {
       const output = document.createElement('div');
-      // console.log(user)
+      
       output.classList.add('card');
+      // Use template literals to display output
       output.innerHTML = 
       `
         <div class="card-img-container">
@@ -27,14 +28,16 @@ fetch ('https://randomuser.me/api/?results=12')
           <p class="card-text">${user.email}</p>
           <p class="card-text cap">${user.location.city} ${user.location.state} </p>
         </div>
-      `  
-       
+      `; 
+      // for each output add an event listener 
       output.addEventListener('click', (e) =>{
+        // Slice the user DOB values into specific variables
         const dob = user.dob.date.slice(0,10); 
         const month = dob.slice(5,7);
         const day = dob.slice(8,11);
         const year = dob.slice(2,4);
         
+        // Use template literals to display the modal conatiner
         modalContainer.innerHTML =
         `
           <div class="modal">
@@ -49,30 +52,32 @@ fetch ('https://randomuser.me/api/?results=12')
                 <p class="modal-text">${user.location.street}, ${user.location.state}, ${user.location.postcode}</p>
                 <p class="modal-text">Birthday:${month}/${day}/${year}</p>
           </div>
-        `
+          <br>
+          <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+          </div>
+        `;
+        // Insert the modal container to be display after click event
         document.body.insertBefore(modalContainer, gallery);
-        const closeBtn = document.querySelector('button').addEventListener('click', (e) =>{
-          document.body.removeChild(modalContainer);
+        // Add event listner to 'X' in modal to remove
+        const closeBtn = document.querySelector('button')
+          .addEventListener('click', (e) =>{
+            document.body.removeChild(modalContainer);
 
         });
 
+        const prevBtn= document.querySelector('#modal-prev').addEventListener('click', () => {});
+        const nextBtn= document.querySelector('#modal-next').addEventListener('click', () => {const next = output.nextSibling;});
+        
       })
-
-      gallery.appendChild(output);   
-    
-   
+      // Add each user object to gallery div 
+      gallery.appendChild(output); 
+      
     });
-
-    // function createModal(users) {    
-    // }
-  //// A way of fetching all data
-  // function fetchData(url){
-  //   return fetch(url)
-  //     .then(res => res.json())
-  // }
-
-
-    });
+ 
+  })
+  .catch( (error) => { console.error("Something went wrong!", error); });
  
 
 
@@ -80,4 +85,5 @@ fetch ('https://randomuser.me/api/?results=12')
   
 
   
+   
 
